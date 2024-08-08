2649d3cb3bae141c7b251fbf6e96b9ea4fb79609
ZhouLestn <zhoulestn@gmail.com>
2024-08-08 17:51:30 +0200

    Hello world!101, VPN NonBasis

--- java_module_test01/src/Main.java
+++ java_module_test01/src/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello world!777, VPN NonBasis");
         System.out.println("Hello world!888, VPN NonBasis");
         System.out.println("Hello world!999, VPN NonBasis");
+        System.out.println("Hello world!101, VPN NonBasis");
     }
 }
