a2b02f024984bd24dd3d368ad9a97900ac8d27fe
ZhouLestn <zhoulestn@gmail.com>
2024-08-13 16:44:03 +0200

    Hello world!105, VPN NonBasis

--- java_module_test01/src/Main.java
+++ java_module_test01/src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!222");
-        System.out.println("Hello world!333");
-        System.out.println("Hello world!444, VPN NonBasis");
-        System.out.println("Hello world!555, VPN NonBasis");
-        System.out.println("Hello world!666, VPN NonBasis");
-        System.out.println("Hello world!777, VPN NonBasis");
-        System.out.println("Hello world!888, VPN NonBasis");
-        System.out.println("Hello world!999, VPN NonBasis");
-        System.out.println("Hello world!104, VPN NonBasis");
-        System.out.println("Hello world!105, VPN NonBasis");
     }
 }
